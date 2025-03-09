023524257e9b2669b10fc2deb0f732e1c45e5955
Shuyibe <shuyibesiddikif@gmail.com>
2025-03-10 03:11:46 +0600

    spinners

--- src/components/shared/Loader.jsx
+++ src/components/shared/Loader.jsx
@@ -1,4 +1,4 @@
-import { ScaleLoader } from 'react-spinners'
+import { RingLoader } from 'react-spinners'
 
 const Loader = () => {
     return (
@@ -11,7 +11,7 @@ const Loader = () => {
       items-center 
     '
         >
-            <ScaleLoader size={100} color='red' />
+            <RingLoader size={100}/>
         </div>
     )
 }
